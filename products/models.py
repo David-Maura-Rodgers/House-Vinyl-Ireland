610aba461fb657ab73d47d87895cb0e27077c96a
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-10-05 18:12:29 +0000

    add: labels json file and start to create the Label model in Models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    '''
+    Model for Labels which each record record is released on
+    '''
+    label = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=150, null=True, blank=True)
