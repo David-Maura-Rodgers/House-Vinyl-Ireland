287d954f35af371c36bbe863a74cfee58faa630e
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-10-05 19:55:29 +0000

    add: admin classes for product app: Label, Record and Image Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Label, Record, Image
 
-# Register your models here.
+
+class LabelAdmin(admin.ModelAdmin):
+    '''
+    Change Label of Record
+    '''
+    list_display = ('friendly_name',)
+    ordering = ('friendly_name',)
+
+
+class RecordAdmin(admin.ModelAdmin):
+    '''
+    Change details of each Record
+    '''
+    list_display = (
+        'artist',
+        'label',
+        'title',
+        'stock_quantity',
+    )
+    ordering = ('artist',)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    '''
+    Change image for each Record
+    '''
+    list_display = ('image_name', 'image', 'record')
+    ordering = ('record',)
+
+
+admin.site.register(Label, LabelAdmin)
+admin.site.register(Record, RecordAdmin)
+admin.site.register(Image, ImageAdmin)
