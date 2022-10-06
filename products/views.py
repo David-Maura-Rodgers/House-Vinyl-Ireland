deca0d42699a577abbf9ddd6b7f08d60501c2abe
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-10-06 20:36:53 +0000

    add: view in products app to display all vinyl records, and basic products template with template variable

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Record
 
-# Create your views here.
+
+def all_products(request):
+    '''
+    A view to display all vinyl records
+    '''
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
