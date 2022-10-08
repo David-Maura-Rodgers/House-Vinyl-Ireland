3613dddc9c69e417aa1c23eae52693d4a61cd65d
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-10-08 23:03:46 +0000

    add/edit: search bar functionality when searching record title and strings in description field / fixed alignment of shopping bag and price display in top nav

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Record
 
 
@@ -8,9 +10,25 @@ def all_products(request):
     '''
 
     records = Record.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!"
+                )
+                return redirect(reverse('products'))
+
+            queries = Q(title__icontains=query) | Q(
+                description__icontains=query
+            )
+            records = records.filter(queries)
 
     context = {
         'records': records,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
