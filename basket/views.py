06c85bf28758d4a16837d35b9366d9c7ebbf8b3e
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-10-12 22:57:15 +0000

    add: views for adjusting and removing items in the basket app, add their corresponding urls too

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import HttpResponse
+from products.models import Record
 
 
 def view_basket(request):
@@ -13,10 +15,10 @@ def add_to_basket(request, item_id):
     '''
     Add a quantity of the specified product/record to the shopping basket
     '''
-
-    quantity = int(request.POST.get('quantity'))
+    product = get_object_or_404(Record, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    quantity = 1
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
@@ -25,3 +27,32 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    '''
+    Adjust the quantity of the specified product to the specified amount
+    '''
+
+    if request.method == 'POST':
+        product = get_object_or_404(Record, pk=item_id)
+        quantity = request.POST.get('quantity')
+        basket = request.session.get('basket', {})
+        basket[item_id] = int(quantity)
+
+        return redirect(reverse('view_basket'))
+        return redirect(redirect_url)
+
+
+def remove_item(request, item_id):
+    '''
+    Delete item from basket
+    '''
+
+    product = get_object_or_404(Record, pk=item_id)
+    basket = request.session.get('basket', {})
+    basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return HttpResponse(status=200)
+    return redirect('view_basket')
