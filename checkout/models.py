08a1535209c250b1a3b6026a6f8b63d3d84b0f64
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-06 19:29:21 +0000

    add: Methods for Order class - generate order number and save method to call generate order number function when an order is saved if it does not already have an order number

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,8 @@
 import uuid
 
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 
 from products.models import Product
 
@@ -28,6 +30,31 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0
     )
+
+    class Meta:
+        '''
+        Order purchases by date
+        '''
+        ordering = ['-purchase_date']
+
+    # prepend _ is used to declare this method is private to this class
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper
+
+    def save(self, *args, **kwargs):
+        '''
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        '''
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
 
 
 class ItemCheckout(models.Model):
