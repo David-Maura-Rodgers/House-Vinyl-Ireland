84373119b39ad3eb633b2fe2db2535fa4556d1e4
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-10 21:46:00 +0000

    add: stripe key variables, error messages and payment intent variable to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -9,21 +9,38 @@ import stripe
 
 
 def checkout(request):
+    '''
+    Use Stripe payment intents to process payment at checkout
+    '''
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
     current_basket = basket_contents(request)
-    total = current_bag['grand_total']
+    total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LgshSHe5KRL462YAIYqkoji9NbTTaw0tVvamFWc6WnpiX6fCJ1SnGxKkEqcOwDwY8aksYJ1PqYOpJRsiiqzoSuD00fPjLY22O',  # noqa
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
