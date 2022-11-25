f58d40324ac3fd21a47f29ae9ec7a6a6abde60cb
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-25 18:07:55 +0000

    add: profile view in profile app, create url in both profile app and at project level url paths

--- profiles/models.py
+++ profiles/models.py
@@ -11,6 +11,7 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     '''
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True
@@ -38,9 +39,10 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
+    '''
     Create or update the user profile
-    """
+    '''
+
     if created:
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
