fcab2692b96d10adc3ba06efe9e67e90934d75c0
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-25 19:37:46 +0000

    edit: make changes to nav item links (names and layouts) removed dropdowns in some cases

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 
 def profile(request):
     '''
     Will display the user's profile
     '''
+
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
     context = {}
