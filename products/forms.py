091e8377a9d37a42e9a3b2a3ddf31ef0783ea9cb
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-27 20:35:26 +0000

    add: widgets file and html file to render the actual image when making changes to existing products

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Record, Label
 
 
@@ -14,6 +15,10 @@ class ProductForm(forms.ModelForm):
 
         model = Record
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
