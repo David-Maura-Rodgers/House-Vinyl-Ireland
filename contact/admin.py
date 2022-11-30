a764d195ff12653c8dda103eec27912a33558ee8
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-11-30 18:18:41 +0000

    add: create Contact Admin class for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    '''
+    Contact fields that will be displayed in Admin
+    '''
+
+    list_display = ('user', 'full_name', 'order_number', 'email', 'message')
+    ordering = ('user',)
+
+
+admin.site.register(Contact, ContactAdmin)
