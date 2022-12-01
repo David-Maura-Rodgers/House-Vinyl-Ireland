15b3839c055a87fe101540806a7ada1aa2f1cd3a
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-12-01 22:08:46 +0000

    add: create ReviewAdmin for review app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    '''
+    Admin panel for review posts
+    '''
+
+    list_display = ('title', 'user', 'status', 'created_on')
+    search_fields = ['title', 'user', 'content']
+    list_filter = ('status', 'title', 'user', 'created_on')
