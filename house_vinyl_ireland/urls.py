f395ea0bda1e1b36eb26cb1fc40f6f4b0df49d83
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-12-12 23:34:33 +0000

    edit: finish 404 html page

--- house_vinyl_ireland/urls.py
+++ house_vinyl_ireland/urls.py
@@ -1,7 +1,7 @@
 '''
 House Vinyl Ireland URL Configuration
 '''
-
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
